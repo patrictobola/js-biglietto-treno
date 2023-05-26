dd8a3c73b22b5b1cec1df9e5743c17d5c42e37b5
patrictobola <tobola92@gmail.com>
2023-05-26 15:31:44 +0200

    Esercizio finito, procedo a ritocchi e bonus

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 console.log('JS OK')
+
+// Definiamo le variabili necessarie allo svolgimento della traccia
+const ticketPlaceholder = document.getElementById('ticketPrice');
+const pricePerKm = 0.21;
+const under18 = 18;
+const over65 = 65;
+const discount18 = 20;
+const discount65 = 40;
+
+console.log(pricePerKm, under18, over65, discount18, discount65)
+
+// Creiamo due prompt per gli input dell'utente 
+const kms = prompt(`Quanti km devi percorrere?`, 100)
+const age = prompt(`Quanti anni devi hai?`, 20)
+
+// Calcoliamo il prezzo al km 
+let finalPrice = 0;
+if (kms) {
+    finalPrice = kms * pricePerKm;
+}
+
+
+console.log(finalPrice)
+
+// Vediamo se possiamo applicare o meno uno sconto 
+if (age > over65) {
+    finalPrice = finalPrice - (finalPrice * discount65 / 100);
+}   else if (age < under18) {
+    finalPrice = finalPrice - (finalPrice * discount18 / 100);
+}
+
+ticketPlaceholder.innerText = finalPrice + '€'
